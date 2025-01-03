d72ba3a4087dd81a2c6b06d9e2c8eb486c759e9f
Vimal  Jain <vimaljain@Vimals-MacBook-Air.local>
2025-01-03 15:32:16 +0530

    add pl;aceholder color

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -67,7 +67,7 @@ const FormValidation = () => {
                             <input className="w-full outline-none text-sm font-bold placeholder-gray" type="text" required name="category" placeholder="Kategorie" value={formData.category} onChange={handleChange} />
                             <img className="rounded-[20px] max-w-[96px] cursor-pointer" src="./assets/images/svg/arrow.svg" alt="dropdown arrow" onClick={toggleDropdown} />
                             {showDropdown && (
-                                <div className="absolute top-full right-0 mt-2 w-48 bg-white border border-lightgray rounded-lg shadow-md">
+                                <div className="absolute top-full right-0 placeholder-gray mt-2 w-48 bg-white border border-lightgray rounded-lg shadow-md">
                                     <ul className="list-none p-2">
                                         <li className="p-2 cursor-pointer" onClick={() => { setFormData({ ...formData, category: "Food" }); setShowDropdown(false); }}>Food </li>
                                         <li className="p-2 cursor-pointer" onClick={() => { setFormData({ ...formData, category: "Travel" }); setShowDropdown(false); }} > Travel </li>
